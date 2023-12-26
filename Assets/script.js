de266b71082e3decc41903570f7027e26814a2b7
kalynsifuentez <kalynsifuentez@gmail.com>
2023-12-26 10:33:40 -0600

    added screenshot added notes to javascript and finsihed Readme

--- Assets/script.js
+++ Assets/script.js
@@ -1,41 +1,23 @@
-
-// Step 1: Input schedule note using elements
-// Step 2: Creating if/else statements. If the user inputs the note how
-// how does it save
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // Added click to "saveBtn"
+  //Variables set for the save button and value of text area.
+  //Variables are within the function with event listener for "saveBtn".
 
   $('.saveBtn').on('click', function (){
     var key = $(this).parent().attr('id')
     var value = $(this).siblings('textarea').val()
     localStorage.setItem(key, value)
   })
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time? Declare items to local storage.
   
-  //Day.js hour code
+  //Day.js hour code:
   let hour =  dayjs().hour();
-// $(".hour").text(hour(12).format("h"));
 
-  // Code to display past, present and future:
+  // Code to compare the current hour and the number value of id attribute "time-block"
   $(".time-block").each(function () {
 
     var schedule = Number($(this).attr("id").split('-')[1]);
  
+    // Conditional statement to represent the past, present, future "time-blocks"
     if (hour === schedule) {
       $(this).addClass("present");
   
@@ -49,23 +31,24 @@ $(function () {
     }
   });
 
- 
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
+  // Code to save user input to localStorage and set
   $('#hour-9 textarea').val(localStorage.getItem('hour-9'))
+  $('#hour-10 textarea').val(localStorage.getItem('hour-10'))
+  $('#hour-11 textarea').val(localStorage.getItem('hour-11'))
+  $('#hour-12 textarea').val(localStorage.getItem('hour-12'))
+  $('#hour-13 textarea').val(localStorage.getItem('hour-13'))
+  $('#hour-14 textarea').val(localStorage.getItem('hour-14'))
+  $('#hour-15 textarea').val(localStorage.getItem('hour-15'))
+  $('#hour-16 textarea').val(localStorage.getItem('hour-16'))
+  $('#hour-17 textarea').val(localStorage.getItem('hour-17'))
+  $('#hour-18 textarea').val(localStorage.getItem('hour-18'))
 
-
-
-  // TODO: Add code to display the current date in the header of the page.
 });
 
+// Day.js to display the current date in the header of the page
 let todaysDate = dayjs();
 
+// Set interval outside of the function, for #currentDay to run without reloading
 setInterval(function(){
   $("#currentDay").text(todaysDate.format("dddd, MMMM D, YYYY h:mm A	"));
 }, 1000)
--- README.md
+++ README.md
@@ -1,21 +1,22 @@
-# # Challenge 4 - Work Day Scheduler Starter Code
+# # Challenge 4 - Work Day Scheduler
 
 ## Description
-
+This application is a functional (9am-6pm) work day scheduler, where users can add text input that will save to local storage. The schedule is color coded as grey for past, red for present, and green for future. There is a date and time at the top of the page that will run as the page is open.
 
 
 ## Code Source 
 
-HTTPS 
+HTTPS: https://kalynsifuentez.github.io/work-day-scheduler/
 
-Live URL: 
+GitHub Repo: https://github.com/kalynsifuentez/work-day-scheduler.git
 
-## Screenshot ![Alt text](Screenshot.png)
+## Screenshot
+![work-day-scheduler-screenshot](Assets/kalynsifuentez.github.io_work-day-scheduler_.png)
 
 ## Acknowledgements
 - https://stackoverflow.com/questions/72512032/how-can-i-compare-the-current-time-with-my-divs-id-900-in-javascript
-
-- Central Tutor Support - Rene Trevino
  - [Awesome Readme Templates](https://awesomeopensource.com/project/elangosundar/awesome-README-templates)
  - [Awesome README](https://github.com/matiassingers/awesome-readme)
  - [How to write a Good readme](https://bulldogjob.com/news/449-how-to-write-a-good-readme-for-your-github-project)
+
+- Central Tutor Support - Rene Trevino
